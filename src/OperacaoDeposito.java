d62ae796fdafe4b336b1cbe7e5fdf07088bee9de
Ana Julia <anajufg127@gmail.com>
2024-07-04 13:30:21 -0300

    Update OperacaoDeposito.java

--- src/OperacaoDeposito.java
+++ src/OperacaoDeposito.java
@@ -4,13 +4,16 @@ public class OperacaoDeposito extends Operacao {
         super('d', valor);
     }
 
+    @Override
     public String toString() {
         String str = this.getData() + "\t" + this.getTipo() + "\t" + this.getValor();
 
         return str;
     }
 
+    @Override
     public double calculaTaxas() {
         return 0;
     }
+
 }
